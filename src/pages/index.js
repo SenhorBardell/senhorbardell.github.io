d1e359b8ff055a701ea7c1b17452196d612df79b
Vlad <senhorbardell@gmail.com>
2018-06-11 12:03:54 +0200

    improve layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,103 +33,106 @@ const IndexPage = props => {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={24} alignContent="flex-start">
-        <Grid item xs sm={12} md={4}>
+      <Grid container alignContent="flex-start">
+        <Grid item xs={12} md={4}>
           <Paper className={classes.paper}>
             <Typography variant="title">Vlad Rimsha</Typography>
             <img src="/static/profile_picture.jpg" width="100%" alt=""/>
 
             <Typography variant="body1">
-              Language agnostic software developer.
+              Language agnostic software engineer since 2008.
+              Best practices and workflows advocate.
+              Productive and disciplined.
+              Traveling nomad and languages lover.
             </Typography>
           </Paper>
         </Grid>
-          <Grid item xs sm={4} md>
-            <Paper className={classes.paper}>
-              <Typography variant="headline">Links</Typography>
-              <List>
-                <ListItem button>
-                  <ListItemText><a className={classes.link} href="https://instagram.com/vlad.rimsha">Instagram (RU)</a></ListItemText>
-                </ListItem>
-                <ListItem button>
-                  <ListItemText><a className={classes.link} href="https://www.linkedin.com/in/влад-римша-9b3a8a95">LinkedIn</a></ListItemText>
-                </ListItem>
-                <ListItem button>
-                  <ListItemText><a className={classes.link} href="mailto://senhorbardell@gmail.com">Mail</a></ListItemText>
-                </ListItem>
-                <ListItem button>
-                  <ListItemText><a className={classes.link} href="https://github.com/senhorbardell">GitHub</a></ListItemText>
-                </ListItem>
-              </List>
-            </Paper>
-          </Grid>
-          <Grid item xs sm={4} md>
-            <Paper className={classes.paper}>
-              <Typography variant="headline">Work History</Typography>
-              <List>
-                <ListItem button>
-                  {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
-                  <ListItemText secondary="lead software engineer">
-                    <a className={classes.link} href="https://www.linkedin.com/company/redq-technologies-inc">RedQ</a>
-                  </ListItemText>
-                </ListItem>
-                <ListItem button>
-                  {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
-                  <ListItemText secondary="senior software engineer">
-                    <a className={classes.link} href="https://www.ambulnz.com">Ambulnz</a>
-                  </ListItemText>
-                </ListItem>
-                <ListItem button>
-                  {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
-                  <ListItemText secondary="owner">
-                    <a className={classes.link} href="https://moshimoshi.io">Moshimoshi</a>
-                  </ListItemText>
-                </ListItem>
-                <ListItem button>
-                  {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
-                  <ListItemText secondary="freelance as senior software engineer">
-                    <a className={classes.link} href="/jobs/upwork">Upwork</a>
-                  </ListItemText>
-                </ListItem>
-                <ListItem button>
-                  {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
-                  <ListItemText secondary="lead software engineer">
-                    <a className={classes.link} href="/jobs/proovela">Proovelab</a>
-                  </ListItemText>
-                </ListItem>
-                <ListItem button>
-                  {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
-                  <ListItemText secondary="lead software engineer">
-                    <a className={classes.link} href="https://appkode.ru/">Kode</a>
-                  </ListItemText>
-                </ListItem>
-              </List>
-            </Paper>
-          </Grid>
-          <Grid item xs sm={4} md={4}>
-            <Paper className={classes.paper}>
-              <Typography variant="headline">Tools & Technologies</Typography>
-              <List>
-                <ListItem>
-                  {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
-                  <ListItemText primary="Python" secondary="Private Libraries maintainer, SQLAlchemy, Flask, PySpark" />
-                </ListItem>
-                <ListItem>
-                  {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
-                  <ListItemText primary="JS" secondary="Fullstack, NodeJS, ES8, React, Angular, Cordova, Phonegap, ReactNative" />
-                </ListItem>
-                <ListItem>
-                  {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
-                  <ListItemText primary="Swift" secondary="Promisekit, ReactiveCocoa" />
-                </ListItem>
-                <ListItem>
-                  {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
-                  <ListItemText primary="Other" secondary="Spark, Go, Java, Android" />
-                </ListItem>
-              </List>
-            </Paper>
-          </Grid>
+        <Grid item xs={12} md={4} lg={2}>
+          <Paper className={classes.paper}>
+            <Typography variant="headline">Links</Typography>
+            <List>
+              <ListItem button>
+                <ListItemText><a className={classes.link} href="https://instagram.com/vlad.rimsha">@vlad.rimsha on Instagram (RU)</a></ListItemText>
+              </ListItem>
+              <ListItem button>
+                <ListItemText><a className={classes.link} href="https://www.linkedin.com/in/влад-римша-9b3a8a95">LinkedIn</a></ListItemText>
+              </ListItem>
+              <ListItem button>
+                <ListItemText><a className={classes.link} href="mailto://senhorbardell@gmail.com">Mail</a></ListItemText>
+              </ListItem>
+              <ListItem button>
+                <ListItemText><a className={classes.link} href="https://github.com/senhorbardell">GitHub</a></ListItemText>
+              </ListItem>
+            </List>
+          </Paper>
         </Grid>
+        <Grid item xs={12} md={4} lg={3}>
+          <Paper className={classes.paper}>
+            <Typography variant="headline">Work History</Typography>
+            <List>
+              <ListItem button>
+                {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
+                <ListItemText secondary="lead software engineer">
+                  <a className={classes.link} href="https://www.linkedin.com/company/redq-technologies-inc">RedQ</a>
+                </ListItemText>
+              </ListItem>
+              <ListItem button>
+                {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
+                <ListItemText secondary="senior software engineer">
+                  <a className={classes.link} href="https://www.ambulnz.com">Ambulnz</a>
+                </ListItemText>
+              </ListItem>
+              <ListItem button>
+                {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
+                <ListItemText secondary="owner">
+                  <a className={classes.link} href="https://moshimoshi.io">Moshimoshi</a>
+                </ListItemText>
+              </ListItem>
+              <ListItem button>
+                {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
+                <ListItemText secondary="freelance as senior software engineer">
+                  <a className={classes.link} href="/jobs/upwork">Upwork</a>
+                </ListItemText>
+              </ListItem>
+              <ListItem button>
+                {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
+                <ListItemText secondary="lead software engineer">
+                  <a className={classes.link} href="/jobs/proovela">Proovelab</a>
+                </ListItemText>
+              </ListItem>
+              <ListItem button>
+                {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
+                <ListItemText secondary="lead software engineer">
+                  <a className={classes.link} href="https://appkode.ru/">Kode</a>
+                </ListItemText>
+              </ListItem>
+            </List>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={4} lg={3}>
+          <Paper className={classes.paper}>
+            <Typography variant="headline">Tools & Technologies</Typography>
+            <List>
+              <ListItem>
+                {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
+                <ListItemText primary="Python" secondary="Private Libraries maintainer, SQLAlchemy, Flask, PySpark" />
+              </ListItem>
+              <ListItem>
+                {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
+                <ListItemText primary="JS" secondary="Fullstack, NodeJS, ES8, React, Angular, Cordova, Phonegap, ReactNative" />
+              </ListItem>
+              <ListItem>
+                {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
+                <ListItemText primary="Swift" secondary="Promisekit, ReactiveCocoa" />
+              </ListItem>
+              <ListItem>
+                {/*<ListItemIcon><InboxIcon/></ListItemIcon>*/}
+                <ListItemText primary="Other" secondary="Spark, Go, Java, Android" />
+              </ListItem>
+            </List>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   )}
 

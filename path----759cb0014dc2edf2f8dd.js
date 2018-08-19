4e14b5f6ced7d96db70a4019be91fc0972bbfa73
Vlad <senhorbardell@gmail.com>
2018-08-19 16:56:27 +0700

    Updates

--- path----759cb0014dc2edf2f8dd.js
+++ path----759cb0014dc2edf2f8dd.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{127:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Vlad Rimsha"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{128:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Vlad Rimsha"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----759cb0014dc2edf2f8dd.js.map
